b9736ad64e7c511dd23f0acdd8c70798be381dbf
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2022-10-12 11:17:14 +0800

    初始化

--- src/main/java/com/example/autologwork/AutoLogWorkApplication.java
+++ src/main/java/com/example/autologwork/AutoLogWorkApplication.java
@@ -1,8 +1,10 @@
-package com.example.test.autologwork;
+package com.example.autologwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class AutoLogWorkApplication {
 
---
+++ src/main/java/com/example/autologwork/AutoLogWorkScheduler.java
@@ -0,0 +1,79 @@
+package com.example.autologwork;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AutoLogWorkScheduler {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${jira.base.url}")
+    private String jiraBaseUrl;
+
+    @Value("${jira.log.work.url}")
+    private String logWrokUrl;
+
+    @Value("${jira.log.work.user.story.id}")
+    private String userStoryId;
+
+    @Value("${jira.log.work.username}")
+    private String username;
+
+    @Value("${jira.log.work.password}")
+    private String password;
+
+    @Value("${jira.log.work.content}")
+    private String content;
+
+    @Scheduled(cron = "${jira.log.work.cron}")
+    public void autoLogWork() {
+        log.info("======= AutoLogWorkScheduler started =======");
+        execute();
+        log.info("======= AutoLogWorkScheduler finished =======");
+    }
+
+    private void execute() {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    jiraBaseUrl + logWrokUrl,
+                    HttpMethod.POST,
+                    requestEntity(username, password, content),
+                    String.class,
+                    userStoryId);
+            log.info("Log work response: {}", response);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            log.info("Send Packet throwable error", throwable);
+        }
+
+    }
+
+    private HttpEntity<?> requestEntity(String username, String password, String content) {
+        HttpHeaders header = new HttpHeaders();
+        header.setBasicAuth(username, password);
+        Map<String, Object> params = new HashMap<>();
+        params.put("timeSpent", "8h");
+        params.put("started", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss.SSS")) + "+0800");
+        params.put("comment", content);
+        log.info("{}", params);
+        return new HttpEntity<>(params, header);
+    }
+}
